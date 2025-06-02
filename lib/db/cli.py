e63b371756e43c8c3a1116503308a1769b8455bb
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-03 01:45:00 +0300

    add main_menu logic & interface, and update books_menu logics

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -7,26 +7,52 @@ from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 def main_menu():
     while True:
         print("\n****Library Management System****")
+        print("1. Manage Books")
+        print("2. Manage Borrowers")
+        print("3. Exit")
+
+        choice = ("Select an option (1-3): ")
+        if choice == '1':
+            book_menu()
+        elif choice == '2':
+            borrower_menu()
+        elif choice == '3':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def book_menu():
+    session = Session()
+    while True:
+        print("\n......Book Management.......")
         print("1. Add Book")
         print("2. Delete Book")
         print("3. View All Books")
-        print("4. Search Book")
-        print("5. Back to Main Menu")
-        print("6. Exit")
+        print("4. View Borrowed Books")
+        print("5. Find Book by Title or ISBN")
+        print("6. Back to Main Menu")
         
-        choice = input("Select an option (1-5): ")
+        choice = input("Select an option (1-6): ")
         if choice == '1':
-            add_book()
+            add_book(session)
         elif choice == '2':
-            delete_book()
+            delete_book(session)
         elif choice == '3':
-            view_books()
+            view_all_books(session)
         elif choice == '4':
-            search_book()
+            view_borrowed_book(session)
         elif choice == '5':
-            main_menu()
+            find_books(session)
         elif choice == '6':
             print("Exiting Books Menu")
             break
         else:
-            print("Invalid choice. Please try again")
+            print("Invalid entry. Insert a choice between 1-6")
+
+
+
+def borrower_menu():
+    while True:
+        pass
