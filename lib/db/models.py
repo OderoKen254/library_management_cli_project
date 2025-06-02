29e2ef4be69918fdc72850a5dc1e296c0b0b217f
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-03 00:14:34 +0300

    feat: add class Books and class Borrower in models and validation logic

--- lib/db/models.py
+++ lib/db/models.py
@@ -5,3 +5,15 @@ from sqlalchemy.orm import relationship, validates
 from database import Base
 import re
 from datetime import datetime
+
+class Book(Base):
+    __tablename__ = "books"
+    pass
+
+
+
+class Borrower(Base):
+    __tablename__ = 'borrowers'
+    pass
+
+
