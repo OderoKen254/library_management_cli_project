26f3d595d3b01e2fc4637df2f683ca0b8fe0e638
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-03 08:41:49 +0300

    update the convention for primary keys, unique keys, ix, and ck

--- lib/db/models.py
+++ lib/db/models.py
@@ -9,6 +9,10 @@ from datetime import datetime
 # Naming convention for foreign keys
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+    "ix": "ix_%(table_name)s_%(column_0_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
 }
 
 # Create metadata with naming convention
